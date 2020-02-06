8bd720933d82b7f23a9d49a0de53d40263ebcd21
Segal Codin <codin.segal@gmail.com>
2020-02-06 13:30:42 +0100

    I have added the server into git

---
+++ Server_Heaven/Assets/Camera_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    public bool Switch_Cameras = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Switch_Cameras == true)
+        {
+            Debug.Log("--------");
+            Switch_BetweenCamera();
+            Switch_Cameras = false;
+        }
+    }
+
+    void Switch_BetweenCamera()
+    {
+        foreach (Transform item in this.transform)
+        {
+            Debug.Log(item);
+            if (item.gameObject.activeSelf == true)
+            {
+                item.gameObject.SetActive(false);
+            }
+            else
+            {
+                item.gameObject.SetActive(true);
+            }
+        }
+    }
+}
